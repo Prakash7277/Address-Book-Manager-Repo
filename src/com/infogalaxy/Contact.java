ed5777b2e056f8f1401f227a59ab1ff33e04d155
Prakash7277 <pkpatil.366@gmail.com>
2022-03-16 14:43:39 +0530

    ADDED: Address Book Management for Company

---
+++ src/com/infogalaxy/Contact.java
@@ -0,0 +1,75 @@
+package com.infogalaxy;
+
+public class Contact {
+    private String fistName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String  mobNo;
+    private String email;
+    private String zip;
+
+    public void setFistName(String fistName){
+        this.fistName = fistName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setCity(String  city){
+        this.city = city;
+    }
+    public void setState(String state){
+        this.state = state;
+    }
+    public void setMobNo(String mobNo){
+        this.mobNo = mobNo;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setZip(String zip){
+        this.zip = zip;
+    }
+    public String getFistName(){
+        return this.fistName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public String getState(){
+        return this.state;
+    }
+    public String getMobNo(){
+        return this.mobNo;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getZip(){
+        return  this.zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "fistName='" + fistName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", mobNo='" + mobNo + '\'' +
+                ", email='" + email + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+}
