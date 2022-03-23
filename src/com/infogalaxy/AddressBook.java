1e144cfa3fae3284c935d3177b53efad62538b36
Prakash7277 <pkpatil.366@gmail.com>
2022-03-23 11:04:25 +0530

    MODIFIED: Adding for Multiple Contact

--- src/com/infogalaxy/AddressBook.java
+++ src/com/infogalaxy/AddressBook.java
@@ -1,11 +1,14 @@
 package com.infogalaxy;
 
+import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
-    Contact contact = new Contact();
+    ArrayList<Contact>  contactlist = new ArrayList();
     public void addContact(){
+        Contact contact = new Contact();
         System.out.println("Enter the First Name : ");
         contact.setFistName(sc.next());
         System.out.println("Enter The Last Name : ");
@@ -22,13 +25,32 @@ public class AddressBook {
         contact.setEmail(sc.next());
         System.out.println("Enter The Zip : ");
         contact.setZip(sc.next());
+        contactlist.add(contact);
     }
     public void displayContact(){
-        System.out.println(contact.toString());
+        for(int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            System.out.println(contact.toString());
+        }
     }
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
-        addressBook.addContact();
-        addressBook.displayContact();
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println("*** CONTACT INVENTORY MANAGEMENT***");
+            System.out.println("1. ADD CONTACT\n2. DISPLAY CONTACT\n3. EXIT");
+            System.out.println("Enter Your Choice :");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.displayContact();
+                    break;
+            }
+        }while(choice != 3);
     }
 }
+
